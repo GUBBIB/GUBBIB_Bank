b438f6229f2f3391206efa5ac30225fcc7d5053a
GUBBIB <moon.dragon250@gmail.com>
2026-08-02 21:42:59 +0900

    마이 정보 반환 api 생성

--- gubbib-bank/src/main/java/com/github/gubbib/gubbibbank/domain/member/controller/MemberController.java
+++ gubbib-bank/src/main/java/com/github/gubbib/gubbibbank/domain/member/controller/MemberController.java
@@ -2,16 +2,16 @@ package com.github.gubbib.gubbibbank.domain.member.controller;
 
 import com.github.gubbib.gubbibbank.domain.auth.dto.LoginResponse;
 import com.github.gubbib.gubbibbank.domain.auth.dto.SignupRequest;
+import com.github.gubbib.gubbibbank.domain.member.dto.MemberResponse;
 import com.github.gubbib.gubbibbank.domain.member.service.MemberService;
+import com.github.gubbib.gubbibbank.security.details.CustomUserDetails;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor(onConstructor_ =  @Autowired)
@@ -20,5 +20,10 @@ public class MemberController {
 
     private final MemberService memberService;
 
-
+    @GetMapping("/me")
+    public ResponseEntity<MemberResponse> me(
+            @AuthenticationPrincipal CustomUserDetails user
+    ){
+        return ResponseEntity.ok().body(MemberResponse.from(user.getMember()));
+    }
 }
